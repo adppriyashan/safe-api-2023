305b75be8e4c40080acd1a93be0122ee933593ad
Pasindu <adpasindupriyashan@outlook.com>
2023-10-23 21:11:57 +0530

    sms

--- app/Http/Controllers/API/DisasterController.php
+++ app/Http/Controllers/API/DisasterController.php
@@ -169,7 +169,7 @@ class DisasterController extends Controller
             $disaster = Disaster::where('id', $request->id)->first();
 
             //send sms
-            foreach (User::where('is_admin', 1)->where('district', $disaster->district)->pluck('contact') as $key => $value) {
+            foreach (User::where('is_admin', 0)->where('district', $disaster->district)->pluck('contact') as $key => $value) {
                 $this->sendNow('Misfortune detected and verified in your district, Please make safe yourself, Please login to SAFE app to more information.', $value);
             }
 
